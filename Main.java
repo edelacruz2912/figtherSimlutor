845542a7acb619b32ce0af0b366e09e3c503a6a1
Eduardo De La Cruz <eduardodelacruz2912@gmail.com>
2018-10-11 18:49:40 -0400

    creating the view Class of the MVC

--- Main.java
+++ Main.java
@@ -8,39 +8,19 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
-	Stage window;
-	
-	Button btn;
+	Stage window;	
+	//Button btn;
 	
 	
 	@Override  //Stage primaryStage = Main Window
 	public void start(Stage primaryStage) {
 		
-		window = primaryStage; 
-		
-		btn = new Button("testing this shit");
-		
-		//layout 
-		StackPane layout = new StackPane();
-		layout.getChildren().add(btn); 
-		
-		//creating the Scene = content of the window
-		//Scene(layout,width,height)
-		Scene scene = new Scene(layout,200,600);
+		window = primaryStage;		
+		WrestlerView wView = new WrestlerView();
+		//scene = contentOfTheWindow
+		Scene scene = new Scene(wView,1000,800);
 		window.setTitle("Wrestler Simulator");// Main window Title.
-		
-		window.setScene(scene);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		window.setScene(scene);		
 		// Display the contents of the scene using the method named show()
 		window.show();
 		
@@ -51,5 +31,14 @@ public class Main extends Application {
 		launch(args); //The start() method is called by the launch(args).
 	}
 
+	
+	
+	//btn = new Button("testing this shit");
+	//layout 
+	//StackPane layoutPane = new StackPane();
+	//layoutPane.getChildren().add(btn); 		
+	//creating the Scene = content of the window
+	//Scene(layoutPane,width,height)
+	//------------------------------------
 	
 }
--- WrestlerView.java
+++ WrestlerView.java
@@ -1,5 +1,15 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
 //This is the View
 //Its only job is to display what the user sees
 //It performs no calculations, but instead passes
@@ -8,6 +18,59 @@ package application;
 
 
 
-public class WrestlerView {
+public class WrestlerView extends BorderPane implements EventHandler<ActionEvent>{
 
+	//Main Pain to all the components and layout.
+	//BorderPane mainPane = new BorderPane();
+
+	//GridPane to hold wrestler.
+	GridPane wrestlerPane = new GridPane();
+		
+	//Button to start the simulation.
+	Button startBtn;
+	
+	//Label
+	Label titleForTheTop ;
+	
+	public WrestlerView()
+	{
+		titleForTheTop = new Label("ROYALRAMBLE");//To have a label in the top.
+		startBtn = new Button("START");//to start the simulation
+		
+		this.setTop(titleForTheTop);		
+		this.setCenter(wrestlerPane);
+		this.setBottom(startBtn);
+		
+		
+	}	
+	//why I can't put this code outside.
+
+	@Override //This method might be move to controllers
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		System.out.println("handle method gets triggered.");
+		if(event.getSource() == startBtn)
+		{
+			System.out.println("button Works");
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+		
+
+	
+
+	
+	
+	
+	
 }
